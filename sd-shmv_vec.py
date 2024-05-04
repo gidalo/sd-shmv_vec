d6b00247aa966f63f645afdd22e0e21bff39377d
GIDALO <46072070+gidalo@users.noreply.github.com>
2024-05-04 22:32:04 +0200

    sd-shmv_vec.py

---
+++ sd-shmv_vec.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2.7
+
+# sd-shmv_vec.py
+#
+# Shutdown script BrewVectrex by MatixVision v1.0 - 2024
+# www.matixvision.it
+#
+# GNU General Public License version 3
+# https://www.gnu.org/licenses/gpl-3.0.en.html
+#
+
+import os
+import time
+import RPi.GPIO as GPIO
+
+from time import sleep
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+# switchPin 6 = GPIO6 (PIN 31 HW)
+switchPin = 6
+
+GPIO.setup(switchPin, GPIO.IN)
+GPIO.input(switchPin)
+while True:
+  input = GPIO.input(switchPin)
+  if (input == 1):
+    os.system("sudo shutdown -h now")
+    time.sleep(0.06)
+GPIO.cleanup()
